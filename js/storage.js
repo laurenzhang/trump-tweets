77f8ff9c35b263c816387bff47bd96443e714eaa
Sungwoo Bae <swbae31@gmail.com>
2017-12-04 00:40:43 -0500

    Implement storing ordered tweets

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,6 @@
 /* DATA SCHEMA (store.get, store.set)
 
-tweets <MAP of tweet objects, ordered by chronological>
+tweets <Dictionary of tweet objects, the link as its key>
 	tweet <Dictionary with data as keys> 
 		tweet_link <string>
 		date	<date object>
@@ -9,63 +9,80 @@ tweets <MAP of tweet objects, ordered by chronological>
 		to <string>
 		related_tweets <list of tweet objects>
 
+recent_ordered_keys <Array of keys for tweets ordered by recent>
+
 past_searches <list of strings>
 	search <string>
 */
 
+a = read_links_from_file('../insult_tweets.txt')
+console.log(a)
 
-//store.clearAll()
-var tweets = get_tweets()
-console.log(tweets)
-
-function get_tweets() {
+function get_tweets(order='recent_ordered') {
 	// TODO: NO LOGIC TO CHECK FOR NEW UPDATES ON TWEETS YET
-	var tweets = []
-
-	// fetch new from twitter
+	
+	// fetch new from twitter and initialize tweets
 	if (store.get('tweets') == undefined) {
-		var links = read_links_from_file()
+		var links = read_links_from_file('asdf')
 		// call back needed
-		for (var link in links) {
-			var tweet = temp_get_tweet(link)
+		var tweets = {}
+		for (var i in links) {
+			var tweet = temp_get_tweet(links[i])
 			// call back needed
-			tweets.push(tweet)
+			tweets[links[i]] = tweet
 		}
-
+		// Create tweets dictionary
 		store.set('tweets', tweets)
 
-	} else { // fetch from local storage
-		tweets = store.get('tweets')
-	}
+		// Create Recent ordered
+		var recent_ordered_keys = Object.keys(tweets).map(function(key) {
+	    	return key;
+		});
+		recent_ordered_keys.sort(function(first, second) {
+	    	return tweets[first].date - tweets[second].date;
+		});
+		store.set('recent_ordered_keys', recent_ordered_keys)
+	} 
 
-	return tweets
+	// Generate ordered tweets and return
+	tweets = store.get('tweets')
+	ordered_keys = store.get(order + '_keys')
+
+	ordered_tweets = []
+	for (i in ordered_keys) {
+		ordered_tweets.push(tweets[ordered_keys[i]])
+	}
+	
+	return ordered_tweets
 }
 
-/* init related tweets from start when getting tweets
 function get_related_tweets(tweet) {
 
-	related_tweets = tweet.related_tweets
+	if (store.get('tweets') == undefined) {
+		get_tweets()
+	}
+	tweets = store.get('tweets')
 
+	related_tweets_links = tweet.related_tweets
+	related_tweets = []
 	// fetch related tweets from twitter
-	if (related_tweets == null) {
-
-		related_tweets = temp_search_for_related_tweets(tweet)
-
-		store.set()
-
-
-	} 
+	for (i in related_tweets_links) {
+		related_tweets.push(tweets[related_tweets_links[i]])
+	}
 	
 	return related_tweets
-}*/
+}
 
 function read_links_from_file(filename) {
 
-	return [1,2,3,4]
+	json = $.getJSON(filename, function(json) {
+    	
+    	return json
+	});
 }
 
 /* TEMP FUNCTIONS THAT WILL BE IN OTHER MODULES IN THE FUTURE */
 
 function temp_get_tweet(link) {
-	return link
+	return {'date' : link, 'aa' : 10000}
 }
