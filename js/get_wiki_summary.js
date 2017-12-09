0caddff4addb7a94ecbeafacb9cd0efefaa80c79
Lauren Zhang <laurenzhang96@gmail.com>
2017-12-08 19:53:48 -0500

    Reformmated get_wiki_summary to use promises

--- js/get_wiki_summary.js
+++ js/get_wiki_summary.js
@@ -1,10 +1,30 @@
-function getWikiSummary(title){
- $.ajax({
+// TODO: integrate w/ the rest of the back-end
+getWikiSummary('Hillary Clinton').then(function(response) {
+  var wiki_content = {};
+  try {
+    pages = response.query.pages;
+
+    for (var page_id in pages) {
+      // Link to article image
+      wiki_content['img_url'] = pages[page_id].thumbnail.source;
+      // Article summary (text before the "Contents" section)
+      wiki_content['summary'] = pages[page_id].extract;
+    }
+  }
+  catch(err) {
+    console.log(err);
+    wiki_content['error'] = 'Wiki summary unavailable';
+  }
+  console.log(wiki_content);
+  // TODO: instead of adding these items to wiki_content, add to tweet object
+  //  - alternatively, display these results directly
+});
+
+function getWikiSummary(title) {
+  return Promise.resolve($.ajax({
     url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=pageimages|extracts&exintro=&explaintext=',
     data: {
       titles: title,
-      // A good example:
-      // titles: 'Hillary Clinton',
       exlimit: 2,
       pithumbsize: 500,
       pilimit: 2
@@ -15,22 +35,7 @@ function getWikiSummary(title){
       withCredentials: true
     },
     success: function(response){
-      console.log(response);
-      
-      try {
-        pages = response.query.pages;
-        for (var page_id in pages) {
-          console.log(title);
-          // Article image
-          console.log(pages[page_id].thumbnail.source);
-          // Article summary (text before the "Contents" section)
-          console.log(pages[page_id].extract);
-        }
-        // TODO: render results on page
-      }
-      catch(err) {
-        console.log('Wiki summary unavailable');
-      }      
+      console.log(response);    
     }
- });
+  }));
 }
