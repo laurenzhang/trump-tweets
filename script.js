9c5f98dec17c9ee7490ac4f74ec626cbe4603a12
Lauren Zhang <laurenzhang96@gmail.com>
2017-12-10 09:59:28 -0500

    Cleaned up populateFeed()

--- script.js
+++ script.js
@@ -14,22 +14,6 @@ $(".dropdown-menu li").click(function() {
     closeMenu(this);
 });
 
-$(document).ready(function() {
-    directory_items = document.getElementsByClassName("directoryitem");
-    for (i in directory_items) {
-        directory_items[i].onclick = function() {
-            $(this).queue(function() {
-                location.href = "./index.html";
-            });
-            $(this).queue(function() {
-                console.log("hey");
-            });
-            $(this).dequeue();
-            $(this).dequeue();
-        };
-    }
-});
-
 function closeMenu(el) {
     $(el).closest('.dropdown').toggleClass("closed").find(".title").text($(el).text());
     $container.css("height", 0);
@@ -54,32 +38,32 @@ function openMenu(el) {
 }
 
 function populateFeed(tweets) {
-    for (i in tweets) {
-        tweet = tweets[i];
-        var table = document.getElementById("table");
-        table.style.visibility = "visible";
+    $(document).ready(function() {
+        for (i in tweets) {
+            if (tweets[i] == undefined) {
+                continue;
+            }
+            tweet = tweets[i];
+            var feed = document.getElementById("feed");
+            
+            // Retrieve tweet ID, content, and date from tweet object
+            var div = document.createElement("div");
+            div.style = "margin-top:30px;clear: both ";
+            div.setAttribute("class", "tweet");
+            div.id = tweet["tweet_id"];
+            // TODO: styling for "p4"
+            var content = document.createElement("p4");
+            content.innerHTML = tweet["tweet_text"];
+            var date = document.createElement("p4");
+            date.innerHTML = tweet["date"];
 
-        var tr = document.createElement("tr");
-        var td = document.createElement("td");
-        
-        // Retrieve tweet ID, content, and date from tweet object
-        var div = document.createElement("div");
-        div.style = "margin-top:30px;clear: both ";
-        div.setAttribute("class", "tweet");
-        div.id = tweet["tweet_id"];
-        // TODO: styling for "p4"
-        var content = document.createElement("p4");
-        content.innerHTML = tweet["tweet_text"];
-        var date = document.createElement("p4");
-        date.innerHTML = tweet["date"];
-
-        // Add tweet corresponding to tweet_id to feed
-        div.appendChild(content);
-        div.appendChild(date);
-        td.appendChild(div);
-        tr.appendChild(td);
-        table.appendChild(tr);
-    }
+            // Add tweet corresponding to tweet_id to feed
+            div.appendChild(content);
+            div.appendChild(document.createElement("br"));
+            div.appendChild(date);
+            feed.appendChild(div);
+        }
+    });
 }
 
 function searchInsultees(insultee) {
