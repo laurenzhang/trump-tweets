e3870a87cff8de432808ef7bb3954126403b6c71
Sungwoo Bae <swbae31@gmail.com>
2017-12-11 23:07:12 -0500

    Implement order to refresh/redirect

--- filter.js
+++ filter.js
@@ -5,24 +5,20 @@ $(document).ready(function() {
 
     ddmenu.onchange = function() {
         var newvalue = ddmenu.options[ddmenu.selectedIndex].value;
-        console.log(newvalue);
 
         var tweets = new Array();
 
-        if (newvalue = 1) {
+        if (newvalue == 1) {
             tweets = get_tweets("recent_ordered").then(function(tweets) {
-                console.log(tweets)
-                populateFeed(tweets);
+                window.location.href = "index.html?order=recent";
             });
-        } else if (newvalue = 2) {
+        } else if (newvalue == 2) {
             tweets = get_tweets("fav_ordered").then(function(tweets) {
-                console.log(tweets)
-                populateFeed(tweets);
+                window.location.href = "index.html?order=fav";
             });
-        } else if (newvalue = 3) {
+        } else if (newvalue == 3) {
             tweets = get_tweets("retweet_ordered").then(function(tweets) {
-                console.log(tweets)
-                populateFeed(tweets);
+                window.location.href = "index.html?order=retweet";
             });
         }
 
